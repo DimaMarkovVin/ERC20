6778d45cc5a718703665c909fc910d543fb27c58
DimaVNTU <34549516+DimaVNTU@users.noreply.github.com>
2017-12-20 14:53:31 +0200

    erc20

---
+++ contracts/MyFirstCoin.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.4.18;
+
+contract ERC20Interface {
+	function totalSupply() constant returns (uint256 totalSupply);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract MyFirstCoin is ERC20Interface {
+	string public constant name = "Dima`s Token";
+	string public constant symbol = "DT";
+	uint8 public constant decimals = 6;
+
+	uint256 _totalSupply = 1000000;
+
+	address public owner;
+
+	mapping (address => uint256) balances;
+
+	mapping (address => mapping (address => uint256)) allowed;
+
+	function MyFirstCoin() public {
+		owner = msg.sender;
+		balances[owner] = _totalSupply;
+	}
+
+
+	function totalSupply() public constant returns (uint256 totalSupply) {
+		totalSupply = _totalSupply;
+		return totalSupply;
+	}
+
+	function balanceOf(address _owner) public constant returns (uint256 balance) {
+		return balances[_owner];
+	}
+
+	function transfer(address _to, uint256 _value) returns (bool success) {
+		if(balances[msg.sender] >= _value && _value > 0 && balances[_to] + _value >= balances[_to]) { 
+			balances[msg.sender] -= _value;
+			balances[_to] += _value;
+			Transfer(msg.sender, _to, _value);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+		if(balances[_from] >= _value && balances[_to] + _value >= balances[_to] && allowed[_from][msg.sender] >= _value) { 
+			balances[_from] -= _value;
+			balances[_to] += _value;
+			Transfer(_from, _to, _value);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	function approve(address _spender, uint256 _value) returns (bool success) {
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+		return allowed[_owner][_spender];
+	}
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+}
